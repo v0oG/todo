c94d0196a007ae28bf418f647a7b38c1bfcbf526
v0oG <youssefelmi97@gmail.com>
2023-07-26 16:55:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import bodyParser from "body-parser"
+
+const app = express();
+const port = 3000;
+const myTodos = [];
+const work = [];
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+  });
+
+app.get("/Work",(req,res)=>{
+    res.render("work.ejs")
+})
+
+app.post("/",(req,res)=>{
+    const newItem = req.body.newItem
+    myTodos.push(newItem)
+    res.render("index.ejs",{addedItem: newItem,myTodos: myTodos})
+    console.log(myTodos)
+})
+
+app.post("/work",(req,res)=>{
+    const newItem = req.body.newItem
+    work.push(newItem)
+    res.render("work.ejs",{addedItem: newItem,work: work})
+    console.log(work);
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
